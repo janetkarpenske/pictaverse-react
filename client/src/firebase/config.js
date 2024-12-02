aaf1bcdf0bad5ed10b1f873f0d50c1aee65c3cae
Janet Karpenske <janetkarpenske@gmail.com>
2024-12-02 07:54:42 -0800

    Update environement variables and firebase config

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOc5PXdi8WjEOl8ZfPb4C92yKgkIKcYc4",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "react-app-36640.firebaseapp.com",
   projectId: "react-app-36640",
   storageBucket: "react-app-36640.firebasestorage.app",
-  messagingSenderId: "976120151755",
-  appId: "1:976120151755:web:8da62da32597e04b44254e",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-6RLQ650M8K"
 };
 
